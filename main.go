52cb47ca85eac2a74c98f0f3637d56b5bac20155
kamkug <kamilkugler@gmail.com>
2020-02-12 22:34:06 +0100

    changes to main.go

--- main.go
+++ main.go
@@ -8,35 +8,75 @@ import (
 
 type Flag struct {
 	//	Flags []string //list of supported flags
+	Id       string
 	Names    []string    // name or names for this flag
 	Defaults interface{} // default value that this flag takes
 	Help     string      // help sentence
-
+	Verify   func(string) bool
 }
 
-type Flags struct {
+type FlagSet struct {
 	SetName string
 	Flags   []Flag
 	Count   int
 }
 
-//CreateFlagStr creates a single flag
-func CreateFlag(n []string, d interface{}, h string) Flag {
+//FlagsValues collects all user defined values for flags and uses default
+//values for not defined|mentioned
+func (fs FlagSet) FlagsValues(args []string) (bool, map[string]interface{}) {
+	fVals := make(map[string]interface{})
+	flagi := fs.Flags
+	for i, _ := range flagi {
+		for _, v1 := range flagi[i].Names {
+			var value string
+			for j, _ := range args {
+				if nextCLIArg := args[j]; nextCLIArg == v1 {
+					if j+1 <= len(args)-1 && args[j+1][0] != 45 {
+						value = args[j+1]
+						//fVals[flagi[i].Id] = args[j+1]          //verify that the particular flag ID has corect input
+
+						//} else {
+						//	fVals[flagi[i].Id] = flagi[i].Defaults
+						//}
+					}
+
+				}
+				fmt.Println(value)
+				if flagi[i].Verify(value) {
+					fVals[flagi[i].Id] = value
+					return true, fVals
+				} else {
+					fVals[flagi[i].Id] = flagi[i].Defaults
+					return true, fVals
+				}
+			}
+		}
+	}
+	//if len(fVals) < len(fs.Flags) {
+	//	return false, fVals
+	//}
+	return true, fVals
+}
+
+//CreateFlag creates a single flag
+func CreateFlag(id string, n []string, d interface{}, h string, v func(string) bool) Flag {
 	flag := Flag{
+		Id:       id,
 		Names:    n,
 		Defaults: d,
 		Help:     h,
+		Verify:   v,
 	}
 	return flag
 }
 
 //PopulateSet adds a single flag to a set
-func PopulateSet(fSet Flags, flag Flag) Flags {
+func PopulateSet(fSet FlagSet, flag Flag) FlagSet {
 
 	length := len(fSet.Flags) + 1
 	set := append(fSet.Flags, flag)
 
-	flagSet := Flags{
+	flagSet := FlagSet{
 		SetName: flag.Names[0],
 		Flags:   set,
 		Count:   length,
@@ -56,7 +96,7 @@ func (e *LengthError) Error() string {
 }
 
 //Verify the arg length
-func (f Flags) VerifyLength(n int) (bool, error) {
+func (f FlagSet) VerifyLength(n int) (bool, error) {
 	if len(os.Args) < (n * 2) {
 		return false, &LengthError{Length: n}
 	}
@@ -66,12 +106,12 @@ func (f Flags) VerifyLength(n int) (bool, error) {
 }
 
 //GetArgs returns the slice of arguments
-func (f Flags) GetArgs() []string {
+func (f FlagSet) GetArgs() []string {
 	return os.Args
 }
 
 //CheckFlag returns the value assigned to the flag
-func (f Flags) CheckFlag(s string) (string, bool) {
+func (f FlagSet) CheckFlag(s string) (string, bool) {
 	argsList := f.GetArgs()
 	for i, arg := range argsList {
 		defer func() {
